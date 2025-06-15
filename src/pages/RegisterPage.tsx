48e59db33437302dd7e97fd02e3b98eb64ba0ea7
lapuxl <svetlana.vdovina2004@gmail.com>
2025-06-15 17:43:37 +0300

    Добавлены правила силы пароля

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -21,9 +21,28 @@ export default function RegisterPage() {
         <Form.Item name="email" label="Email" rules={[{ required: true, type: 'email' }]}>
           <Input />
         </Form.Item>
-        <Form.Item name="password" label="Пароль" rules={[{ required: true }]}>
-          <Input.Password />
-        </Form.Item>
+        <Form.Item
+  name="password"
+  label="Пароль"
+  rules={[
+    { required: true, message: 'Введите пароль' },
+    {
+      validator: (_, value) => {
+        if (!value) return Promise.resolve()
+
+        const strongPasswordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+[\]{};':"\\|,.<>/?]).{8,}$/
+        if (!strongPasswordRegex.test(value)) {
+          return Promise.reject(
+            new Error('Пароль должен быть не короче 8 символов, содержать заглавную букву, цифру и спецсимвол')
+          )
+        }
+        return Promise.resolve()
+      }
+    }
+  ]}
+>
+  <Input.Password />
+</Form.Item>
         <Form.Item>
           <Button type="primary" htmlType="submit" block>Зарегистрироваться</Button>
         </Form.Item>
